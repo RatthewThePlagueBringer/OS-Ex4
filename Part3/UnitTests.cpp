f72e9447f75b693546ead285f4c7bdc94d73a36d
RatthewThePlagueBringer <matthewgrimaldi111@gmail.com>
2024-10-04 17:38:11 -0400

    Update UnitTests.cpp

--- Part3/UnitTests.cpp
+++ Part3/UnitTests.cpp
@@ -1,4 +1,12 @@
-#include "gtest/gtest.h"
 #include "BackEnd.h"
+#include <vector>
+#include <string>
 
-//Write your unit tests here
+using namespace std;
+
+Airport airport1 = Airport("AP1");
+Airport airport2 = Airport("AP2");
+Airport airport3 = Airport("AP3");
+
+Flight flight1 = Flight(20, 5, airport1, airport2);
+
